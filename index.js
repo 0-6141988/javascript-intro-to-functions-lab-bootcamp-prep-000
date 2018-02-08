df4de3f4733f835c672ead221f54007533ce85ed
Julian Driver <ijulian.driver@gmail.com>
2018-02-08 01:26:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,11 @@ function shout (string) {
      return "I can't hear you!"
    } else if(string == string.toUpperCase()){
      return"YES INDEED!"
+   } else if(string == "I love you, Grandma.") {
+     return " I love you, too."
    }
  }
+ 
  
 
  
